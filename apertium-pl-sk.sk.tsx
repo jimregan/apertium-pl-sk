51ba094d769e42999125cf85b694647e1a5ccc72
Jimmy O Regan <jimregan@users.sourceforge.net>
2009-12-23 23:02:49 +0000

    add a basic tsx file for the few + entries in the analyser now
    git-svn-id: svn://svn.code.sf.net/p/apertium/svn/incubator/apertium-pl-sk@18066 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ apertium-pl-sk.sk.tsx
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tagger name="slovakian">
+<tagset>
+  <def-label name="PRE" closed="true">
+    <tags-item lemma="pre" tags="pr"/>
+  </def-label> 
+  <def-mult name="PRENEHOEMPHNT" closed="true">  
+    <sequence>
+      <label-item label="PRE"/>
+      <tags-item tags="prn.emph.p3.nt.sg.acc"/>
+    </sequence>
+  </def-mult>
+  <def-mult name="PRENEHOEMPHM" closed="true">  
+    <sequence>
+      <label-item label="PRE"/>
+      <tags-item tags="prn.emph.p3.m.sg.acc"/>
+    </sequence>
+  </def-mult>
+  <def-mult name="PRENEHONT" closed="true">  
+    <sequence>
+      <label-item label="PRE"/>
+      <tags-item tags="prn.p3.nt.sg.acc"/>
+    </sequence>
+  </def-mult>
+  <def-mult name="PRENEHOM" closed="true">  
+    <sequence>
+      <label-item label="PRE"/>
+      <tags-item tags="prn.p3.m.sg.acc"/>
+    </sequence>
+  </def-mult>
+
+</tagset>
+</tagger>
